a11e04dfde0f2b6ddf21d46a3f98a54b040c4f5c
Alessandro Cresto Miseroglio <alex179ohm@gmail.com>
2019-12-11 18:33:07 +0100

    Add From for NsqResult

--- src/result.rs
+++ src/result.rs
@@ -26,14 +26,14 @@ use crate::response::Response;
 
 pub type NsqResult<T> = Result<T, NsqError>;
 
-impl Into<NsqResult<Response>> for Response {
-    fn into(self) -> NsqResult<Response> {
-        NsqResult::Ok(self)
+impl From<Response> for NsqResult<Response> {
+    fn from(r: Response) -> NsqResult<Response> {
+        NsqResult::Ok(r)
     }
 }
 
-impl Into<NsqResult<Response>> for NsqError {
-    fn into(self) -> NsqResult<Response> {
-        NsqResult::Err(self)
+impl From<NsqError> for NsqResult<Response> {
+    fn from(e: NsqError) -> NsqResult<Response> {
+        NsqResult::Err(e)
     }
 }
